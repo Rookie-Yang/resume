e0a9cc4d279388e5ea69472eaab0d519aca05b32
RookieYang <yy335281965@gmail.com>
2018-10-18 22:57:59 +0800

    添加导航条

--- js/main.js
+++ js/main.js
@@ -19,5 +19,14 @@ window.onscroll = function(xxx){
   else{
     TopNavBar.classList.remove('sticky')    
   }
-  
+}
+
+let liTags = document.querySelectorAll('nav.menu > ul > li')
+for(let i=0;i<liTags.length;i++){
+  liTags[i].onmouseenter = function(x){
+    x.currentTarget.classList.add('active')
+  }
+  liTags[i].onmouseleave = function(x){
+    x.currentTarget.classList.remove('active')
+  }
 }
